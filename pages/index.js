3ca3eb2d325d728479c5344b213ff9989788c40e
Brian Kung <brian@callmekung.com>
2018-12-28 00:09:16 -0600

    Tiled inventory items

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 
 import InventoryBody from '../lib/inventory_body'
+import Navbar from '../lib/navbar'
 
 export default class extends React.Component {
   render() {
     return (
       <div id="base">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css" />
+          <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" />
         </Head>
+
+        <Navbar />
 
         <InventoryBody />
       </div>
