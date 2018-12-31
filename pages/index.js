5d617539d10fea287da6b87c8f0eeb54f1cb2f0a
Brian Kung <brian@callmekung.com>
2018-12-30 18:02:33 -0600

    Redux WIP - hooks up Navbar to store

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import Head from 'next/head'
 
+import { Provider } from 'react-redux'
+import store from '../lib/redux/store'
+
 import InventoryBody from '../lib/inventory_body'
 import Navbar from '../lib/navbar'
 
 export default class extends React.Component {
   render() {
     return (
-      <div id="base">
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css" />
-          <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" />
-        </Head>
+      <Provider store={store}>
+        <div id="base">
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css" />
+            <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" />
+          </Head>
 
-        <Navbar />
+          <Navbar />
 
-        <InventoryBody />
-      </div>
+          <InventoryBody />
+        </div>
+      </Provider>
     );
   }
 }
